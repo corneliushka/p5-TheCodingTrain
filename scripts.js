5f9ade37595439da153fa48271f33bb82cbb4ee4
corneliushka <corneliu.gaina@gmail.com>
2019-03-22 09:58:51 +0100

    init p5

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Creer un tableau avec deux objets, contenant les noms et notes
+// Il faut faire la moyenne des deux notes
+// il faut sortir la personne qui a la plus grande note. 
+//
+//
+//
+//
